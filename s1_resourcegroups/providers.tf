1aa340ff198d51e9b7fe31d40bd41e775313a1ee
aaronstone9805 <aaronstone9805@gmail.com>
2025-08-17 16:01:35 -0400

    Update provider configurations and add key vault module

--- s1_resourcegroups/providers.tf
+++ s1_resourcegroups/providers.tf
@@ -1,6 +1,14 @@
 terraform {
+  required_version = ">= 1.9, < 2.0"
   required_providers {
-    azurerm = "=3.100.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.117"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
 }
 
--- s1_resourcegroups/resouregroup.tf
+++ s1_resourcegroups/resouregroup.tf
@@ -4,3 +4,42 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   tags = local.common_tags
 }
+
+# We need the tenant id for the key vault.
+data "azurerm_client_config" "this" {}
+
+# This allows us to randomize the region for the resource group.
+# module "regions" {
+#   source  = "Azure/avm-utl-regions/azurerm"
+#   version = "0.3.0"
+# }
+
+# This allows us to randomize the region for the resource group.
+# resource "random_integer" "region_index" {
+#   max = length(module.regions.regions) - 1
+#   min = 0
+# }
+
+# This ensures we have unique CAF compliant names for our resources.
+# module "naming" {
+#   source  = "Azure/naming/azurerm"
+#   version = "0.3.0"
+# }
+
+# # This is required for resource modules
+# resource "azurerm_resource_group" "this" {
+#   location = module.regions.regions[random_integer.region_index.result].name
+#   name     = module.naming.resource_group.name_unique
+# }
+
+# This is the module call
+module "keyvault" {
+  # source = "../../"
+
+  location = var.resource_group_location
+  source              = "Azure/avm-res-keyvault-vault/azurerm"
+  name                = kv-test-module
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.this.tenant_id
+  enable_telemetry    = false
+}
